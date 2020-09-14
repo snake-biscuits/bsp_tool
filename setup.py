2c76a84615ed6cc9b5017f07e7cf6a77f3e61938
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2020-09-14 13:04:10 +1000

    misnamed variable in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     author="Jared Ketterer",
     author_email="haveanotherbiscuit@gmail.com",
     long_description=readme_text,
-    long_description="text/markdown",
+    long_description_content_type="text/markdown",
     url="https://github.com/snake-biscuits/bsp_tool",
     download_url="https://github.com/snake-biscuits/bsp_tool/archive/v0.1.0.tar.gz",
     keywords=["source", "bsp", "valve"],
