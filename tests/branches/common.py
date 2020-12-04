cc4c673223b63a4c06f047696ee2e297c020bb57
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2020-12-04 22:23:31 +1000

    updating tests to use pytest

--- tests/branches/common.py
+++ tests/branches/common.py
@@ -1,36 +1,42 @@
 import struct
-import unittest
 
 from bsp_tool.branches import common
 
 
-class TestBaseMethods(unittest.TestCase):
+class Example(common.Base):
+    __slots__ = ["id", "position", "data"]
+    _format = "i3f4i"
+    _arrays = {"position": [*"xyz"], "data": 4}
 
-    def setUp(self):
-        class Example(common.Base):
-            __slots__ = ["id", "position", "data"]
-            _format = "i3f4i"
-            _arrays = {"position": [*"xyz"], "data": 4}
-        self.Example = Example
 
-    def test_flat(self):
-        # test_unpack
+class TestBase:
+    def test_unpack(self):
+        raw_struct = b"\x00\x00\x00\x00" b"\xDE\xAD\xBE\xEF" \
+                     b"\xDE\xAD\xBE\xEF" b"\xDE\xAD\xBE\xEF" \
+                     b"\x00\x00\x00\x04" b"\x00\x00\x00\x05" \
+                     b"\x00\x00\x00\x06" b"\x00\x00\x00\x07"
+        raw_tuple = struct.unpack(self.Example._format, raw_struct)
+        test_struct = self.Example(raw_tuple)
+
+        assert test_struct.id == 0
+        # assert test_struct.position == common.MappedArray([*"xyz"])
+
+    def test_pack(self):
         raw_struct = b"\x00\x00\x00\x00" b"\x00\x00\x00\x01" \
                      b"\x00\x00\x00\x02" b"\x00\x00\x00\x03" \
                      b"\x00\x00\x00\x04" b"\x00\x00\x00\x05" \
                      b"\x00\x00\x00\x06" b"\x00\x00\x00\x07"
         raw_tuple = struct.unpack(self.Example._format, raw_struct)
         test_struct = self.Example(raw_tuple)
-        # test_pack
         flattened_struct = test_struct.flat()
         recreated_struct = struct.pack(self.Example._format, *flattened_struct)
-        self.assertEqual(raw_struct, recreated_struct)
+        assert raw_struct == recreated_struct
 
 
-class TestMappedArrayMethods(unittest.TestCase):
-
-    def setUp(self):
-        self.sample_A = common.MappedArray([0, 1, 2])
-        self.sample_B = common.MappedArray([3, 4, 5], ["a", "b", "c"])
-        self.sample_C = common.MappedArray([6, 7, 8, 9], {"D": ["i", "ii"],
-                                                           "E": ["iii", " iv"]})
+class TestMappedArray:
+    def test_init(self):
+        sample_A = common.MappedArray([0, 1, 2])
+        sample_B = common.MappedArray([3, 4, 5], ["a", "b", "c"])
+        sample_C = common.MappedArray([6, 7, 8, 9], {"D": ["i", "ii"],
+                                                     "E": ["iii", " iv"]})
+        assert (sample_A, sample_B, sample_C) != (None, None, None)
--- tests/bsp.py
+++ tests/bsp.py
@@ -1,21 +1,17 @@
-import unittest
-
 from bsp_tool import Bsp
 
 
-class TestBspImport(unittest.TestCase):
-
-    def setUp(self):
+class TestBspImport:
+    def set_up(self):
         self.test2_bsp = Bsp("tests/maps/test2.bsp")
         self.upward_bsp = Bsp("tests/maps/pl_upward.bsp")
 
     def test_no_errors(self):
+        self.set_up()
         assert len(self.test2_bsp.log) == 0
+        assert len(self.upward_bsp.log) == 0
 
     def test_entites_loaded(self):
+        self.set_up()
         assert self.test2_bsp.ENTITIES[0]["classname"] == "worldspawn"
         assert self.upward_bsp.ENTITIES[0]["classname"] == "worldspawn"
-
-    def tearDown(self):
-        del self.test2_bsp
-        del self.upward_bsp
