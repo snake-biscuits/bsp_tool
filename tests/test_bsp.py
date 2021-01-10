b87328a09defda2047720f8fa2961731b0f188b3
repl.it user <replituser@example.com>
2021-01-10 03:08:34 +0000

    updated tests to run much faster

--- tests/test_bsp.py
+++ tests/test_bsp.py
@@ -2,24 +2,32 @@ from bsp_tool import load_bsp
 from bsp_tool import IdTechBsp, ValveBsp
 
 
-def test_load_bsp():
-    test2_bsp = load_bsp("tests/maps/test2.bsp")
-    upward_bsp = load_bsp("tests/maps/pl_upward.bsp")
-    bigbox_bsp = load_bsp("tests/maps/test_bigbox.bsp")
-    assert isinstance(test2_bsp, ValveBsp)
-    assert isinstance(upward_bsp, ValveBsp)
-    assert isinstance(bigbox_bsp, IdTechBsp)
+global bsps
+bsps = dict()
+
+
+def test_load_bsp():  # must run first!
+    bsps["test2"] = load_bsp("tests/maps/test2.bsp")
+    bsps["upward"] = load_bsp("tests/maps/pl_upward.bsp")
+    bsps["bigbox"] = load_bsp("tests/maps/test_bigbox.bsp")
+    assert isinstance(bsps["test2"], ValveBsp)
+    assert isinstance(bsps["upward"], ValveBsp)
+    assert isinstance(bsps["bigbox"], IdTechBsp)
+
+
+class TestIdTechBsp:
+    def test_no_errors(self):
+        assert len(bsps["bigbox"].loading_errors) == 0
+
+    def test_entities_loaded(self):
+        assert bsps["bigbox"].ENTITIES[0]["classname"] == "worldspawn"
 
 
 class TestValveBsp:
     def test_no_errors(self):
-        test2_bsp = ValveBsp(filename="tests/maps/test2.bsp")
-        upward_bsp = ValveBsp(filename="tests/maps/pl_upward.bsp")
-        assert len(test2_bsp.loading_errors) == 0
-        assert len(upward_bsp.loading_errors) == 0
+        assert len(bsps["test2"].loading_errors) == 0
+        assert len(bsps["upward"].loading_errors) == 0
 
     def test_entites_loaded(self):
-        test2_bsp = ValveBsp(filename="tests/maps/test2.bsp")
-        upward_bsp = ValveBsp(filename="tests/maps/pl_upward.bsp")
-        assert test2_bsp.ENTITIES[0]["classname"] == "worldspawn"
-        assert upward_bsp.ENTITIES[0]["classname"] == "worldspawn"
+        assert bsps["test2"].ENTITIES[0]["classname"] == "worldspawn"
+        assert bsps["upward"].ENTITIES[0]["classname"] == "worldspawn"
--- tests/test_id_software.py
+++ tests/test_id_software.py
@@ -4,7 +4,7 @@ from bsp_tool import IdTechBsp
 from bsp_tool.branches.id_software import quake3
 
 
-def test_face_struct():  # most complex branches.base.Mappedarray
+def test_face_struct():  # most complex branches.base.MappedArray
     bigbox_bsp = IdTechBsp(quake3, "tests/maps/test_bigbox.bsp")
     header = bigbox_bsp.HEADERS["FACES"]
     with open(bigbox_bsp.file.name, "rb") as file:
