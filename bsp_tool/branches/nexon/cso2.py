4d6dd0ddba55e965a384722c6def593b7b1cae74
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2021-03-20 14:48:17 +1000

    copying vindictus format for CSO2 (untested)

--- bsp_tool/branches/nexon/cso2.py
+++ bsp_tool/branches/nexon/cso2.py
@@ -1,7 +1,14 @@
 from . import vindictus
 
 
-BSP_VERSION = 100  # unknown
+BSP_VERSION = 100  # assumed
 
 LUMP = vindictus.LUMP
 lump_header_address = vindictus.lump_header_address
+LumpHeader = vindictus.LumpHeader
+read_lump_header = vindictus.read_lump_header
+
+LUMP_CLASSES = vindictus.LUMP_CLASSES.copy()
+SPECIAL_LUMP_CLASSES = vindictus.SPECIAL_LUMP_CLASSES.copy()
+
+methods = vindictus.methods
