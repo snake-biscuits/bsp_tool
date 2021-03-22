2a383423864ad1c25823b1b963480fbf85936ca2
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2021-03-22 14:03:12 +1000

    Source 2 does not use .bsp, it instead uses the .vwrld format

--- bsp_tool/branches/valve/__init__.py
+++ bsp_tool/branches/valve/__init__.py
@@ -4,8 +4,6 @@ __all__ = ["cs_go", "cs_source", "orange_box"]
 from . import cs_go
 from . import cs_source
 from . import orange_box  # Most Source Engine Games
-# TODO: Source 2 (does source 2 use .bsp?)
 
 __doc__ = """Valve Software developed the GoldSrc Engine, building on Id Software's Quake & Quake II Engines.
-This variant powered Half-Life & CS:1.6. In developing Half-Life 2 they created the Source Engine.
-Later developing the Source 2 Engine and partially porting DOTA2 & CS:GO to Source 2."""
+This variant powered Half-Life & CS:1.6. In developing Half-Life 2 they created the Source Engine."""
