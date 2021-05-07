0f4a8afa151f715f1c498de9f58757fd2ec2e40f
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2021-05-07 17:49:01 +1000

    sketching out a function to generate documentation

--- bsp_tool/branches/__init__.py
+++ bsp_tool/branches/__init__.py
@@ -71,3 +71,35 @@ by_version = {
     valve.cs_go.BSP_VERSION: valve.cs_go,
     valve.orange_box.BSP_VERSION: valve.orange_box  # 20 (many sub-variants)
              }
+
+
+def gen_docs():  # hardcoded to RespawnBsp
+    bsp_format_name = "RespawnBsp (Respawn Entertainment)"
+    print(f"# {bsp_format_name} Supported Games (v0.3.0)")
+    print("| Bsp version | Game | Branch script | Lumps supported |")
+
+    game_scripts = {"Counter-Strike: Source": valve.orange_box}
+    # ^ {"game": script}
+    # NOTE: markdown looks the same without padding
+    game_name_padding = max(len(g) for g in game_scripts.keys())
+    game_script_padding = max(len(s.__name__) for s in game_scripts.values()) + 2
+    print(f"| --: | {'-' * game_name_padding} | {'-' * game_script_padding} | ------: |")
+    for game in sorted(game_scripts, key=lambda g: game_scripts[g].BSP_VERSION):
+        game_script = game_scripts[game]
+        supported_lumps = len({*game_script.BASIC_LUMP_CLASSES,
+                               *game_script.LUMP_CLASSES,
+                               *game_script.SPECIAL_LUMP_CLASSES})
+        # TODO: Pad each line equally (optional)
+        print(f"| {game_script.BSP_VERSION:>4s}",
+              f"| {game} | `{game_script.__name__}`",
+              f"| {supported_lumps} / {len(game_script.LUMPS)} |")
+
+    print("| Lump index | Bsp version | Lump name | Lump version | LumpClass | % of struct mapped |")
+    # TODO: print each lump entry, in order of BSP_VERSION
+    # TODO: if a script uses a different format, add it to the list
+    # TODO: print alternate lump names
+    # TODO: don't print lump name or index if it doesn't change
+    """| 4 |  20 | `VISIBILITY` | 0 | :x:                     |   0% |
+       | 5 |  20 | `NODES`      | 0 | `valve.orange_box.Node` | 100% |
+       |   |  20 |              | 0 | `nexon.vindictus.Node`  | 100% |"""
+    # NOTE: % mapped is based on number of unknowns in format
