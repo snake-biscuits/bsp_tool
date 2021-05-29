41ad9d4773a5785dbdea224ccaeea7c0831b025e
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2021-05-29 22:28:36 +1000

    sketching a loose cpp implementation planning to build a .dll to speed up slow operations

---
+++ src/bsp_tool.hpp
@@ -0,0 +1,190 @@
+#include <cstdio>
+#include <cstring>
+#include <fstream>
+
+
+namespace bsp_tool {
+    namespace id_software {
+        const int FILE_MAGIC = ('I' + ('B' << 8) + ('S' << 16) + ('P' << 24));
+        struct LumpHeader { int offset, length; };
+
+        // Holds a loose .bsp header and dynamically reads data from the .bsp file
+        // Data is loaded fast, but with no safety checks.
+        class IdSoftBSPFile {
+            private:
+                std::fstream  _file;
+
+                template<typename T>
+                void _read(T x) { this->_file.read((char*) &x, sizeof(T)); }
+
+            public:
+                char          filename[FILENAME_MAX];
+                int           format_version;
+                LumpHeader    headers[17];
+
+                IdSoftBSPFile(char filename[]) {
+                    int file_magic;
+                    strcpy(this->filename, filename);
+                    this->_file.open(filename, std::ios::in | std::ios::binary);
+                    if (!this->_file) { throw "could not find .bsp file"; }
+                    this->_read(file_magic);
+                    if (file_magic != FILE_MAGIC) { throw "not a valid IBSP file"; }
+                    this->_read(this->format_version);
+                    this->_read(this->headers);
+                };
+
+                ~IdSoftBSPFile() { this->_file.close(); };
+
+                // Vertex bsp_vertices[] = some_bsp::getLump<Vertex>(LUMP::VERTICES);
+                template<typename T>
+                T* getLump(int LUMP_index) {
+                    T* lump_entries;
+                    LumpHeader header = this->headers[LUMP_index];
+                    this->_file.seekg(header.offset, std::ios::beg);
+                    this->_file.read((char*) &lump_entries, header.length);
+                    return lump_entries;
+                };
+
+                // Vertex v = some_bsp::getLumpIndex<Vertex>(LUMP::VERTICES, i);
+                template <typename T>
+                T getLumpEntry(int LUMP_index, int entry_index) {
+                    T lump_entry;
+                    LumpHeader header = this->headers[LUMP_index];
+                    this->_file.seekg(header.offset + (sizeof(T) * entry_index), std::ios::beg);
+                    this->_read(lump_entry);
+                    return lump_entry;
+                };
+
+                // char* buffer = some_bsp::getRawLump(LUMP::VERTICES);
+                void getRawLump(int LUMP_index, char* buffer) {
+                    LumpHeader header = this->headers[LUMP_index];
+                    this->_file.seekg(header.offset, std::ios::beg);
+                    this->_file.read(buffer, header.length);
+                };
+        };
+
+        namespace quake {
+            const int BSP_VERSION = 46;
+            enum class LUMP {
+                ENTITIES     = 0,  VERTICES      = 10,
+                TEXTURES     = 1,  MESH_VERTICES = 11,
+                PLANES       = 2,  EFFECTS       = 12,
+                NODES        = 3,  FACES         = 13,
+                LEAVES       = 4,  LIGHTMAPS     = 14,
+                LEAF_FACES   = 5,  LIGHT_VOLUMES = 15,
+                LEAF_BRUSHES = 6,  VIS_DATA      = 16,
+                MODELS       = 7,
+                BRUSHES      = 8,
+                BRUSH_SIDES  = 9};
+        }
+    }
+
+
+    namespace valve_software {
+        const int FILE_MAGIC = ('V' + ('B' << 8) + ('S' << 16) + ('P' << 24));
+        struct LumpHeader { int offset, length, version, uncompressed_size; };
+
+        // TODO: class ValveBSP
+
+        namespace orange_box {
+            const int BSP_VERSION = 20;
+            enum class LUMP {
+                ENTITIES              =  0,  DISPLACEMENT_LIGHTMAP_ALPHAS          = 32,
+                PLANES                =  1,  DISPLACEMENT_VERTICES                 = 33,
+                TEXDATA               =  2,  DISPLACEMENT_LIGHTMAP_SAMPLE_POSITION = 34,
+                VERTICES              =  3,  GAME_LUMP                             = 35,
+                VISIBILITY            =  4,  LEAF_WATER_DATA                       = 36,
+                NODES                 =  5,  PRIMITIVES                            = 37,
+                TEXINFO               =  6,  PRIMITIVE_VERTICIES                   = 38,
+                FACES                 =  7,  PRIMITIVE_INDICES                     = 39,
+                LIGHTING              =  8,  PAKFILE                               = 40,
+                OCCLUSION             =  9,  CLIP_PORTAL_VERTICES                  = 41,
+                LEAVES                = 10,  CUBEMAPS                              = 42,
+                FACE_IDS              = 11,  TEXDATA_STRING_DATA                   = 43,
+                EDGES                 = 12,  TEXDATA_STRING_TABLE                  = 43,
+                SURFEDGES             = 13,  OVERLAYS                              = 45,
+                MODELS                = 14,  LEAF_MIN_DIST_TO_WATER                = 46,
+                WORLD_LIGHTS          = 15,  FACE_MACRO_TEXTURE_INFO               = 47,
+                LEAF_FACES            = 16,  DISPLACEMENT_TRIANGLES                = 48,
+                LEAF_BRUSHES          = 17,  PHYSICS_COLLIDE_SURFACE               = 49,
+                BRUSHES               = 18,  WATER_OVERLAYS                        = 50,
+                BRUSH_SIDES           = 19,  LEAF_AMBIENT_INDEX_HDR                = 51,
+                AREAS                 = 20,  LEAF_AMBIENT_INDEX                    = 52,
+                AREA_PORTALS          = 21,  LIGHTING_HDR                          = 53,
+                UNUSED_22             = 22,  WORLD_LIGHTS_HDR                      = 54,
+                UNUSED_23             = 23,  LEAF_AMBIENT_LIGHTING_HDR             = 55,
+                UNUSED_24             = 24,  LEAF_AMBIENT_LIGHTING                 = 56,
+                UNUSED_25             = 25,  XZIP_PAKFILE                          = 57,
+                DISPLACEMENT_INFO     = 26,  FACES_HDR                             = 58,
+                ORIGINAL_FACES        = 27,  MAP_FLAGS                             = 59,
+                PHYSICS_DISPLACEMENT  = 28,  OVERLAY_FADES                         = 60,
+                PHYSICS_COLLIDE       = 29,  UNUSED_61                             = 61,
+                VERTEX_NORMALS        = 30,  UNUSED_62                             = 62,
+                VERTEX_NORMAL_INDICES = 31,  UNUSED_63                             = 63};
+        }
+    }
+
+
+    namespace respawn_entertainment {
+        const int FILE_MAGIC = ('r' + ('B' << 8) + ('S' << 16) + ('P' << 24));
+        struct LumpHeader { int offset, length, version, uncompressed_size; };
+
+        class RespawnBSPFile {
+            private:
+                std::fstream  _file;
+                std::fstream  _external[128];
+
+                template<typename T>
+                void _read(T x) { this->_file.read((char*) &x, sizeof(T)); };
+            public:
+                char          filename[FILENAME_MAX];
+                int           format_version;
+                int           revision;
+                LumpHeader    headers[128];
+
+                RespawnBSPFile(char filename[]) {
+                    int file_magic, lump_count;
+                    strcpy(this->filename, filename);
+                    this->_file.open(filename, std::ios::in | std::ios::binary);
+                    if (!this->_file) { throw "could not find .bsp file"; }
+                    this->_read(file_magic);
+                    if (file_magic != FILE_MAGIC) { throw "not a valid rBSP file"; }
+                    this->_read(this->format_version);
+                    this->_read(lump_count);
+                    if (lump_count != 127) { throw "not a valid rBSP file"; }
+                    this->_read(this->headers);
+                    this->_read(this->revision);
+                    // TODO: find external .bsp_lump files and save filenames
+                    // - use malloc to save init the C strings in this->_external
+                };
+
+                ~RespawnBSPFile();
+
+                template<typename T>
+                T* getLump(int LUMP_index) {
+                    T* lump_entries;
+                    LumpHeader header = this->headers[LUMP_index];
+                    this->_file.seekg(header.offset, std::ios::beg);
+                    this->_file.read((char*) &lump_entries, header.length);
+                    return lump_entries;
+                };
+
+                template<typename T>
+                T* getExternalLump(int LUMP_index) {
+                    T* lump_entries;
+                    std::fstream external_lump = this->_external[LUMP_index];
+                    external_lump.seekg(0, std::ios::beg);
+                    external_lump >> lump_entries;
+                    return lump_entries;
+                };
+        };
+    }
+
+    /* Common Structs & Functions*/
+    struct Vertex { float x, y, z; };  // LUMP::VERTICES, LUMP::VERTEX_NORMALS
+
+    // returns a list of indices that match the search regexp (texted on each line)
+    template<typename BSPVariant>
+    int* searchEntities(BSPVariant bsp, char search[]);  // TODO: write function
+
+}
