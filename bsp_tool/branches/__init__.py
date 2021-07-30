ade7e06e3735fbda5b926083e6f7118a022b6f69
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2021-07-30 21:00:36 +1000

    noting known VBSP versions listed on the Valve Developer Community site

--- bsp_tool/branches/__init__.py
+++ bsp_tool/branches/__init__.py
@@ -67,4 +67,42 @@ by_version = {
     valve.cs_source.BSP_VERSION: valve.cs_source,
     valve.cs_go.BSP_VERSION: valve.cs_go,
     valve.orange_box.BSP_VERSION: valve.orange_box  # 20 (many sub-variants)
+    # TODO: test if any of the following games require new branch scripts
+    # 17: troika.vtmb  # Vampire: The Masquerade - Bloodlines
+    # 17: valve.hl2_beta
+    # 18: ritual.sin_emergence
+    # ??: ritual.cscz_sp  # Counter-Strike: Condition Zero - Deleted Scenes
+    # ??: ritual.sin  # 1998 original, Quake 2 engine
+    # ??: royal_rudius.hdtf  # Royal Rudius Entertaiment: Hunt Down the Freeman  # pls no
+    # 18: valve.hl2_beta
+    # 19: valve.css  # Counter-Strike: Source
+    # 19: valve.dod_s  # Day of Defeat: Source
+    # 19: valve.hl2
+    # 19: valve.hl2_dm  # Half-Life 2: Deathmatch
+    # NOTE: orange_box should cover most v20 VBSPs
+    # 20: ace.zeno_clash  # ACE Team: Zeno Clash (http://www.moddb.com/games/zeno-clash/downloads/zeno-clash-sdk/)
+    # 20: arkane.dark_messiah  # Arkane Studios: Dark Messiah of Might and Magic
+    # 20: crowbar.black_mesa  # Crowbar Collective: Black Mesa
+    # 20: nexon.vindictus
+    # 20: outerlight.bloody_good_time
+    # 20: outerlight.the_ship
+    # 20: valve.gmod
+    # 20: valve.hl2_ep1
+    # 20: valve.hl2_ep2
+    # 20: valve.hl2_lc  # Half-Life 2: Lost Coast
+    # 20: valve.l4d  # Left 4 Dead
+    # 20: valve.portal
+    # 20: valve.tf2  # Team Fortress 2
+    # 21: briscoe.dear_esther  # Robert Briscoe: Dear Esther
+    # 21: dwraden.begginer  # Davey Wraden: The Beginner's Guide
+    # 21: dwraden.stanley_parable  # 2013 SDK mod?
+    # 21: new_world.insurgency  # New World Interactive: Insurgency
+    # 21: valve.alien_swarm
+    # 21: valve.csgo
+    # 21: valve.l4d2  # Left 4 Dead 2
+    # 21: valve.portal_2
+    # 22: valve.dota_2  # early beta?
+    # 23: valve.dota_2  # ported to Source 2 now?
+    # 27: monochrome.contagion
+    # ??: fix_korea.tactical_intervention  # good luck getting your hands on a copy
              }
