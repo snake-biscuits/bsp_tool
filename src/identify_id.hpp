1370509731f034e70ba3d11930e1a9793df1a1a3
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2021-08-25 03:22:42 +0100

    splitting src/identify_id.hpp tests into their own .cpp file

--- src/identify_id.hpp
+++ src/identify_id.hpp
@@ -19,26 +19,3 @@ bool matchID(std::string id, int id_checksum) {
     return (*id_as_int == id_checksum);
 };
 
-
-// g++ identify_id.hpp -o test_identify_id.exe -DTEST
-#ifdef TEST
-int main() {
-    std::string  string_IBSP ("IBSP");
-    bool         is_IBSP = matchID(string_IBSP, IBSP);
-    printf("bool(%s == %i) = %s", string_IBSP.c_str(), IBSP, is_IBSP ? "true" : "false");
-
-    std::string  string_VBSP ("VBSP");
-    bool         is_VBSP = matchID(string_VBSP, VBSP);
-    printf("bool(%s == %i) = %s", string_VBSP.c_str(), VBSP, is_VBSP ? "true" : "false");
-
-    std::string  string_rBSP ("rBSP");
-    bool         is_rBSP = matchID(string_rBSP, rBSP);
-    printf("bool(%s == %i) = %s", string_rBSP.c_str(), rBSP, is_rBSP ? "true" : "false");
-
-    if ( !is_IBSP || !is_VBSP || !is_rBSP ) {
-        return 1;
-    }
-
-    return 0;
-};
-#endif
