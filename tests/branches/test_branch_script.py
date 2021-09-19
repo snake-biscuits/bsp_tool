8e983784254512107d2164e3998c57eef6f9ce29
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2021-09-19 19:48:47 +1000

    fixing broken test

--- tests/branches/test_branch_script.py
+++ tests/branches/test_branch_script.py
@@ -1,3 +1,4 @@
+import inspect
 from types import ModuleType
 from typing import List
 import warnings
@@ -13,11 +14,13 @@ def LumpClasses_of(module: ModuleType) -> List[object]:
     attrs = dir(module)
     for a in attrs:
         LumpClass = getattr(module, a)
-        if issubclass(LumpClass, (base.Struct, base.MappedArray)):
-            yield LumpClass
+        if inspect.isclass(LumpClass):
+            if issubclass(LumpClass, (base.Struct, base.MappedArray)):
+                yield LumpClass
 
 
-basic_branch_scripts = [id_software.quake, gearbox.bshift, valve.goldsrc]
+basic_branch_scripts = [id_software.quake, id_software.quake2, id_software.quake3,
+                        infinity_ward.call_of_duty1, gearbox.bshift, valve.goldsrc]
 
 
 @pytest.mark.parametrize("branch_script", basic_branch_scripts)
@@ -27,12 +30,10 @@ def test_quake_branch_script(branch_script):
     unused_LumpClasses = set(LumpClasses_of(branch_script)).difference(used_LumpClasses)
     if len(unused_LumpClasses) > 0:
         warning_text = "\n".join(["Unused LumpClasses in branch script:", *[lc.__name__ for lc in unused_LumpClasses]])
-        warnings.warn(warnings.UserWarning(warning_text))
+        warnings.warn(UserWarning(warning_text))
 
 
-branch_scripts = [arkane.dark_messiah, gearbox.bshift,
-                  id_software.quake, id_software.quake, id_software.quake,
-                  infinity_ward.call_of_duty1,
+branch_scripts = [arkane.dark_messiah,
                   nexon.cso2, nexon.cso2_2018, nexon.vindictus,
                   respawn.titanfall, respawn.titanfall2, respawn.apex_legends]
 # TODO: Ritual Entertainment - Übertools
@@ -45,11 +46,11 @@ def test_branch_script(branch_script):
         # ^ {"LUMP": {version: LumpClass}}
         used_LumpClasses.update(version_dict.values())
     # ignore StaticPropClass
-    used_LumpClasses = set(filter(lambda lc: not lc.__name__.startswith("StaticProp", used_LumpClasses)))
+    used_LumpClasses = set(filter(lambda lc: not lc.__name__.startswith("StaticProp"), used_LumpClasses))
     unused_LumpClasses = set(LumpClasses_of(branch_script)).difference(used_LumpClasses)
     if len(unused_LumpClasses) > 0:
         warning_text = "\n".join(["Unused LumpClasses in branch script:", *[lc.__name__ for lc in unused_LumpClasses]])
-        warnings.warn(warnings.UserWarning(warning_text))
+        warnings.warn(UserWarning(warning_text))
 
 
 # TODO: use maplist to look at all lump headers and ensure unused lumps are correctly marked
