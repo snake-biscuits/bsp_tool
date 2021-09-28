8fa13a774f74b017a3c0ba88ce50feecc1c214e6
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2021-09-28 23:32:28 +1000

    moving unused / old extensions to bsp_tool-examples

--- bsp_tool/extensions/viewer/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ["camera", "qt5_viewer", "render", "sdl2_viewer", "vector"]
-
-from . import camera
-from . import qt5_viewer
-from . import render
-from . import sdl2_viewer
-from . import vector
