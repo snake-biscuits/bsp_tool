f8b24fc835aeb7ccabb4acdf4aa359cc34e0caa5
r-ex <67599507+r-ex@users.noreply.github.com>
2021-12-20 19:13:41 +0000

    Added missing slot for apex_legends VertexLitBump

--- bsp_tool/branches/respawn/apex_legends.py
+++ bsp_tool/branches/respawn/apex_legends.py
@@ -347,7 +347,7 @@ class VertexLitBump(base.Struct):  # LUMP 73 (0049)
     negative_one: int  # -1
     uv1: List[float]  # lightmap coords
     colour: List[int]
-    __slots__ = ["position_index", "normal_index", "uv0", "negative_one", "uv1"]
+    __slots__ = ["position_index", "normal_index", "uv0", "negative_one", "uv1", "colour"]
     _format = "2I2fi2f4B"  # 32 bytes
     _arrays = {"uv0": [*"uv"], "uv1": [*"uv"], "colour": [*"rgba"]}
 
