35b0602bf13046623d72fe1ea134f1f3cec40524
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2022-01-09 14:16:28 +1000

    cleaned up the respawn compile log extractor

--- extract_logs.py
+++ extract_logs.py
@@ -1,5 +1,6 @@
 import fnmatch
 import os
+import shutil
 
 import bsp_tool
 from tests.maplist import installed_games
@@ -13,13 +14,21 @@ log_paths = {"r1_logs": ("E:/Mod", "Titanfall"),
 for log_dir, base_dir in log_paths.items():
     map_dirs = installed_games[base_dir]
     for map_dir in map_dirs:
-        map_dir = os.path.join(*base_dir, map_dir)
-        print(f"Searching {map_dir} ...")
-        for map_name in fnmatch.filter(os.listdir(map_dir), "*.bsp"):
-            bsp = bsp_tool.load_bsp(os.path.join(map_dir, map_name))
+        full_map_dir = os.path.join(*base_dir, map_dir)
+        print(f"Searching {full_map_dir} ...")
+        for map_name in fnmatch.filter(os.listdir(full_map_dir), "*.bsp"):
+            bsp = bsp_tool.load_bsp(os.path.join(full_map_dir, map_name))
             if not hasattr(bsp, "PAKFILE"):
                 continue
+            # print all file extensions
+            # extensions = sorted({os.path.splitext(f)[1] for f in bsp.PAKFILE.namelist()})
+            # print(f"  {bsp.filename:<32s} {' '.join(extensions)}")
             logs = fnmatch.filter(bsp.PAKFILE.namelist(), "*.txt")
-            for logfile in logs:
-                print(f"Extracting {logfile} from {bsp.filename} ...")
-                bsp.PAKFILE.extract(logfile, path=log_dir)
+            for log_file in logs:
+                print(f"Extracting {log_file} from {bsp.filename} ...")
+                bsp.PAKFILE.extract(log_file, path=log_dir)
+                ext_filename = os.path.join(log_dir, log_file)
+                ext_dir, ext_file = os.path.split(ext_filename)
+                new_filename = os.path.join(ext_dir, f"{map_dir.replace('/', '.')}.{ext_file}")
+                shutil.move(ext_filename, new_filename)
+                print(f"\-> Renamed to {new_filename}")
