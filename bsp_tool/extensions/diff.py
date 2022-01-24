5cd07c9ed6391451c47a1716e9dd2692dc9cec80
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2022-01-24 14:28:23 +1000

    fiddling with diff

--- bsp_tool/extensions/diff.py
+++ bsp_tool/extensions/diff.py
@@ -1,4 +1,5 @@
 """Run with 64-bit python! Respawn .bsp files are large!"""
+# TODO: write to log file
 import difflib
 import itertools
 import os
@@ -8,6 +9,10 @@ from typing import Iterable
 from .. import RespawnBsp
 from ..branches.respawn import titanfall, titanfall2
 
+
+r1_dir = "E:/Mod/Titanfall/maps"
+r1o_dir = "E:/Mod/TitanfallOnline/maps"
+r2_dir = "E:/Mod/Titanfall2/maps"
 
 shared_maps = [("mp_angel_city", "mp_angel_city"),
                ("mp_colony", "mp_colony02"),
@@ -21,6 +26,11 @@ def diff_bsps(bsp1, bsp2, full=False):
     for lump1, lump2 in zip(bsp1.branch.LUMP, bsp2.branch.LUMP):
         lump1 = lump1.name
         lump2 = lump2.name
+
+        if lump1 not in bsp1.headers or lump2 not in bsp2.headers:
+            continue  # lazy fix for rbsp externals
+            # TODO: note absent headers (not just for respawn.ExternalLumpManager!)
+
         bsp1_header = bsp1.headers[lump1]
         bsp2_header = bsp2.headers[lump2]
 
@@ -109,13 +119,13 @@ def dump_headers(maplist):  # just for r1 / r1o / r2  (Titanfall Games)
     for r1_filename, r2_filename in maplist:
         print(r1_filename.upper())
 
-        r1o_map_exists = os.path.exists(f"E:/Mod/TitanfallOnline/maps/{r1_filename}.bsp")
+        r1o_map_exists = os.path.exists(os.path.join(r1o_dir, f"{r1_filename}.bsp"))
         # TODO: do close matches exist?
 
-        r1_map = RespawnBsp(titanfall, f"E:/Mod/Titanfall/maps/{r1_filename}.bsp")
+        r1_map = RespawnBsp(titanfall, os.path.join(r1_dir, f"{r1_filename}.bsp"))
         if r1o_map_exists:
-            r1o_map = RespawnBsp(titanfall, f"E:/Mod/TitanfallOnline/maps/{r1_filename}.bsp")
-        r2_map = RespawnBsp(titanfall2, f"E:/Mod/Titanfall2/maps/{r2_filename}.bsp")
+            r1o_map = RespawnBsp(titanfall, os.path.join(r1o_dir, f"{r1_filename}.bsp"))
+        r2_map = RespawnBsp(titanfall2, os.path.join(r2_dir, f"{r2_filename}.bsp"))
 
         for i in range(128):
             r1_lump = titanfall.LUMP(i)
@@ -156,17 +166,3 @@ def xxd(data: bytes, width: int = 32) -> str:
         hex = " ".join([f"{b:02X}" for b in _bytes])
         ascii = "".join([chr(b) if re.match(r"[a-zA-Z0-9/\\]", chr(b)) else "." for b in _bytes])
         yield f"{address}:  {hex}  {ascii}"
-
-
-if __name__ == "__main__":
-    # r1_relic = RespawnBsp(titanfall, "E:/Mod/Titanfall/maps/mp_relic.bsp")
-    # r1o_relic = RespawnBsp(titanfall, "E:/Mod/TitanfallOnline/maps/mp_relic.bsp")
-    # r2_relic = RespawnBsp(titanfall2, "E:/Mod/Titanfall2/maps/mp_relic02.bsp")
-    # diff_respawn_bsps(r1_relic, r1o_relic)  # IDENTICAL!
-
-    # r1_angel = RespawnBsp(titanfall, "E:/Mod/Titanfall/maps/mp_angel_city.bsp")
-    # r1o_angel = RespawnBsp(titanfall, "E:/Mod/TitanfallOnline/maps/mp_angel_city.bsp")
-    # r2_angel = RespawnBsp(titanfall2, "E:/Mod/Titanfall2/maps/mp_angel_city.bsp")
-    # diff_respawn_bsps(r1_angel, r1o_angel)  # slight differences! interesting!
-
-    dump_headers(shared_maps)
