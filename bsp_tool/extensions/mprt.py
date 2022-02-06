07f9f6b014c55a2f3f2b47086ce6a58e529f46a3
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2022-02-07 08:12:06 +1000

    updating .mprt exporter to match Legion's `RBspLib::ExportPropContainer`

--- bsp_tool/extensions/mprt.py
+++ bsp_tool/extensions/mprt.py
@@ -1,12 +1,14 @@
 import struct
 
 
-def generate(bsp):
-    with open(f"{bsp.filename.split('.')[0]}.mprt", "wb") as mprt_file:
+def generate(bsp, filename=""):
+    if filename == "":
+        filename = f"{bsp.filename.split('.')[0]}.mprt"
+    with open(filename, "wb") as mprt_file:
         mprt_file.write(b"mprtFAKE LOL")
         for prop in bsp.GAME_LUMP.sprp.props:
             mprt_file.write(bsp.GAME_LUMP.sprp.model_names[prop.model_name].encode() + b"\0")
             position = prop.origin
-            rotation = (prop.angles[2], prop.angles[0], 90 + prop.angles[1])
+            rotation = (prop.angles[2], prop.angles[0], prop.angles[1])
             scale = prop.scale
             mprt_file.write(struct.pack("7f", *position, *rotation, scale))
