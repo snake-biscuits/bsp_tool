e7b3688e2c557e1db6725a1fd98babac3d1cd044
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2022-05-04 14:04:07 +0100

    identified titanfall.MeshBounds.unknown_1 (radius)

--- bsp_tool/branches/respawn/titanfall.py
+++ bsp_tool/branches/respawn/titanfall.py
@@ -334,11 +334,10 @@ class Mesh(base.Struct):  # LUMP 80 (0050)
 
 class MeshBounds(base.Struct):  # LUMP 81 (0051)
     origin: List[float]
-    unknown_1: float  # might be related to volume / surface area?
+    radius: float  # approx. magnitude of extents
     extents: List[float]  # bounds extend symmetrically by this much along each axis
-    # NOTE: both unknown_1 & extents are always positive
     unknown_2: int  # could be a float, but value is strange; unsure of purpose; can be 0
-    __slots__ = ["origin", "unknown_1", "extents", "unknown_2"]
+    __slots__ = ["origin", "radius", "extents", "unknown_2"]
     _format = "4f3fI"
     _arrays = {"origin": [*"xyz"], "extents": [*"xyz"]}
 
