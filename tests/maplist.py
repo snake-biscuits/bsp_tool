fdb634e79d8f1585ca3fe1bf6e56334304bd64ad
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2022-11-30 10:52:28 +0000

    adding titanfall2 test maps for #47 to tests/maplist.py

--- tests/maplist.py
+++ tests/maplist.py
@@ -422,7 +422,7 @@ group_dirs = {"C:/Program Files (x86)/Steam/steamapps/sourcemods": sourcemod_dir
 # registering tests/maps first
 installed_games = {("./", "tests/maps"): ["Call of Duty 4", "Call of Duty 4/mp",
                                           "Quake", "Quake 2", "Quake 3 Arena",
-                                          "Team Fortress 2"]}
+                                          "Team Fortress 2", "Titanfall 2"]}
 for group, games in group_dirs.items():
     if os.path.exists(group):
         for game, map_dirs in games.items():
