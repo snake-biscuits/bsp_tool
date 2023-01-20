548f8a3773ec2022867d2712facc9e2ce031f3fb
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2023-01-20 20:07:39 +0000

    fixing up `extensions.mprt` to load correctly & quickly

--- bsp_tool/extensions/mprt.py
+++ bsp_tool/extensions/mprt.py
@@ -5,9 +5,12 @@ def generate(bsp, filename=""):
     if filename == "":
         filename = f"{bsp.filename.split('.')[0]}.mprt"
     with open(filename, "wb") as mprt_file:
-        mprt_file.write(b"mprtFAKE LOL")
+        mprt_file.write(b"mprtFAKE")
+        mprt_file.write(len(bsp.GAME_LUMP.sprp.props).to_bytes(4, "little"))
         for prop in bsp.GAME_LUMP.sprp.props:
-            mprt_file.write(bsp.GAME_LUMP.sprp.model_names[prop.model_name].encode() + b"\0")
+            model_name = bsp.GAME_LUMP.sprp.model_names[prop.model_name].replace("\\", "/").rpartition("/")[2]
+            model_name = model_name[:-4]  # strip .mdl from tail
+            mprt_file.write(model_name.encode() + b"\0")
             position = prop.origin
             rotation = (prop.angles[2], prop.angles[0], prop.angles[1])
             scale = prop.scale
