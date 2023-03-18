f555d0c394b847a935a146ca5875b575b28b159a
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2023-03-18 20:25:32 +1000

    (#88) Testing Python 3.11 support

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Games/Entertainment :: First Person Shooters",
         "Topic :: Multimedia :: Graphics :: 3D Modeling",
         "Topic :: Software Development :: Libraries :: Python Modules"
