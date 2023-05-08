b60e1fae923b0a2882635be593122d37198eec5d
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2023-05-09 01:27:35 +1000

    (#23) test (xfail) for implicit changes bug

--- tests/test_lumps.py
+++ tests/test_lumps.py
@@ -1,4 +1,8 @@
+import collections
+import io
+
 from bsp_tool import lumps
+from bsp_tool.branches import base
 
 import pytest
 
@@ -25,3 +29,27 @@ class TestRemapSlice:
         assert lumps._remap_slice(slice(0, 50, None), 50) == slice(0, 50, 1)
         assert lumps._remap_slice(slice(0, 50, 1), 50) == slice(0, 50, 1)
         assert lumps._remap_slice(slice(0, 69, 1), 50) == slice(0, 50, 1)
+
+
+class TestDecompress:
+    # TODO: test decompression on a repacked ValveBsp
+    ...
+
+
+LumpHeader_basic = collections.namedtuple("basic", ["offset", "length"])
+
+
+class LumpClass_basic(base.MappedArray):
+    _mapping = [*"xyz"]
+    _format = "3H"
+
+
+class TestBspLump:
+    @pytest.mark.xfail(reason="not yet implemented")
+    def test_implicit_change(self):
+        header = LumpHeader_basic(offset=0, length=6)
+        stream = io.BytesIO(b"\x01\x00\x02\x00\x03\x00")
+        lump = lumps.BspLump.from_header(stream, header, LumpClass_basic)
+        assert lump[0].x == 1
+        lump[0].x += 1
+        assert lump[0].x == 2
