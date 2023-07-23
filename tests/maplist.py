85aa69ddbd194e6d1c4b0a8ddda97b666aed49c3
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2023-07-23 06:43:16 +0100

    (MegaTest) adding Apex Legends season 17 event map patch

--- tests/maplist.py
+++ tests/maplist.py
@@ -382,7 +382,10 @@ extracted_dirs = {
                                  "9may23/depot/r5-170/game/r2/maps",  # 10 maps | 1.74 GB | .vpk
                                  "20jun23/maps",  # 14 maps | 2.81 GB | .vpk
                                  "20jun23/depot/r5-170/game/r2/maps",  # 10 maps | 1.74 GB | .vpk
-                                 "20jun23/depot/r5-171/game/r2/maps"],  # 11 maps | 2.21 GB | .vpk
+                                 "20jun23/depot/r5-171/game/r2/maps",  # 11 maps | 2.21 GB | .vpk
+                                 "19jul23/maps",  # 2 maps | 169 MB | .vpk
+                                 "19jul23/depot/r5-170/game/r2/maps",  # 1 map | 6 MB | .vpk
+                                 "19jul23/depot/r5-171/game/r2/maps"],  # 2 maps | 168 MB | .vpk
         # Nintendo Switch Release
         "ApexLegends/switch": ["season9/maps",  # 7 maps | 2.4 GB | .vpk
                                "season9/depot/r5-90/game/r2/maps"],  # 7 maps | 2.3 GB | .vpk
