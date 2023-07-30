2dd966d06206dd6a9913411a22d194544f189e5e
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2023-07-30 16:14:36 +1000

    (tests) (extensions.diff) planning tests for `VisibilityDiff`

---
+++ tests/extensions/diff/id_software/test_quake2.py
@@ -0,0 +1,8 @@
+# from bsp_tool.branches.id_software.quake2 import Visibility
+# from bsp_tool.extensions.diff.id_software.quake2 import VisibilityDiff
+
+
+class TestVisibilityDiff:
+    # TODO: short_stats
+    # TODO: 0xFF vs 0xDC with same num_clusters
+    ...
