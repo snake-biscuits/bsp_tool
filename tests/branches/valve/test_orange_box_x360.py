fc4632c8e83237ab9f41b6c50cd3e9ad37ae92b4
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2023-10-20 07:58:29 +0100

    (#60 & #87) first `valve.orange_box_x360` map & tests

---
+++ tests/branches/valve/test_orange_box_x360.py
@@ -0,0 +1,19 @@
+from ... import utils
+from bsp_tool import ValveBsp
+from bsp_tool.branches.valve import orange_box_x360
+
+
+bsps = utils.get_test_maps(ValveBsp, {
+    orange_box_x360: ["Xbox360/The Orange Box"]})
+
+
+class TestMethods:
+    ...
+
+    # TODO:
+    # -- leaves_of_node
+    # -- textures_of_brush
+    # -- vertices_of_displacement
+    # --- NOTE: DisplacementInfo_x360 hasn't been mapped yet
+    # -- vertices_of_face
+    # -- worldspawn_volume
--- tests/test_ValveBsp.py
+++ tests/test_ValveBsp.py
@@ -2,12 +2,17 @@ from . import utils
 from bsp_tool import ValveBsp
 from bsp_tool.branches.strata import strata
 from bsp_tool.branches.valve import orange_box
+from bsp_tool.branches.valve import orange_box_x360
 
 import pytest
 
 
-bsps = utils.get_test_maps(ValveBsp, {orange_box: ["Team Fortress 2"],
-                                      strata: ["Momentum Mod"]})
+bsps = utils.get_test_maps(ValveBsp, {
+    orange_box: ["Team Fortress 2"],
+    strata: ["Momentum Mod"]})
+
+x360_bsps = utils.get_test_maps(ValveBsp, {
+    orange_box_x360: ["Xbox360/The Orange Box"]})
 
 
 @pytest.mark.parametrize("bsp", bsps.values(), ids=bsps.keys())
@@ -19,3 +24,15 @@ def test_no_errors(bsp):
 @pytest.mark.parametrize("bsp", bsps.values(), ids=bsps.keys())
 def test_entities_loaded(bsp):
     assert bsp.ENTITIES[0]["classname"] == "worldspawn"
+
+
+@pytest.mark.parametrize("bsp", x360_bsps.values(), ids=x360_bsps.keys())
+def test_x360(bsp):
+    assert len(bsp.loading_errors) == 0
+
+
+# NOTE: Official X360/OrangeBox maps don't have this issue
+@pytest.mark.xfail(raises=AssertionError, reason="invalid GAME_LUMP.sprp")
+@pytest.mark.parametrize("bsp", x360_bsps.values(), ids=x360_bsps.keys())
+def test_x360_failing(bsp):
+    assert len(bsp.GAME_LUMP.loading_errors) == 0
