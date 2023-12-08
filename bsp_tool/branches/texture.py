f8bb534b8bf33b62b48e36ae88f09acc4a40ba41
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2023-12-08 13:44:06 +1000

    `TextureVector` class with methods

---
+++ bsp_tool/branches/texture.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from . import vector
+
+
+# TODO: Image class (Issue #159)
+
+class ProjectionAxis:
+    axis: vector.vec3
+    offset: float
+    scale: float
+
+    def __init__(self, axis: vector.vec3, offset: float = None, scale: float = None):
+        self.axis = vector.vec3(*axis)
+        self.offset = 0 if offset is None else offset
+        self.scale = 1 if scale is None else scale
+
+    def project(self, point: vector.vec3) -> float:
+        return (vector.dot(point, self.axis) + self.offset) / self.scale
+
+
+class TextureVector:
+    s: ProjectionAxis
+    t: ProjectionAxis
+
+    def __init__(self, s: ProjectionAxis = None, t: ProjectionAxis = None):
+        self.s = ProjectionAxis([1, 0]) if s is None else s
+        self.t = ProjectionAxis([0, 1]) if t is None else t
+
+    def uv_at(self, point: vector.vec3) -> vector.vec2:
+        u = self.s.project(point)
+        v = self.t.project(point)
+        return vector.vec2(u, v)
+
+    @classmethod
+    def from_normal(cls, normal: vector.vec3) -> TextureVector:
+        axes = [(vector.vec3(y=1), vector.vec3(z=-1)),  # X: east / west wall
+                (vector.vec3(x=1), vector.vec3(z=-1)),  # Y: north / south wall
+                (vector.vec3(x=1), vector.vec3(y=-1))]  # Z: floor / ceiling
+        # defaults to floor / ceiling if equally on each axis
+        best_axis = 2 if len(set(normal)) == 0 else list(normal).index(max(normal))
+        s, t = axes[best_axis]
+        return cls(s, t)
