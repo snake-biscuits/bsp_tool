444abf19f2d00ff0575674823e70af53196c1f8c
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2024-02-21 20:49:10 +0000

    (IW Engine) geo export

--- bsp_tool/branches/infinity_ward/call_of_duty1_demo.py
+++ bsp_tool/branches/infinity_ward/call_of_duty1_demo.py
@@ -4,9 +4,11 @@ import struct
 from typing import List
 
 from ...utils import editor
+from ...utils import geometry
 from ...utils import physics
 from ...utils import vector
 from .. import base
+from .. import colour
 from .. import shared
 from ..id_software import quake
 from ..id_software import quake3  # CoD1 was built on RTCW
@@ -60,10 +62,29 @@ class LUMP(enum.Enum):
 LumpHeader = quake.LumpHeader
 
 
-# TODO: known lump changes from Quake 3 -> CoD 1:
+# TODO: known lump changes from Quake 3 -> CoD 1 Demo:
 
 
 # TODO: a rough map of the relationships between lumps:
+#                    /-> Texture
+# Brush -> BrushSides -> Plane
+#      \-> Texture
+
+# Cell -> Portals
+#     \-> CullGroupIndices
+
+#       /-> Plane
+# Portal -> Cell
+#       \-> PortalVertices
+
+#      /-> TriangleSoups
+# Model -> LeafFaces -> PatchCollision
+#      \-> Brushes -> BrushSides
+
+# Leaf -> LeafFaces -> PatchCollision
+#     \-> LeafBrushes -> Brushes -> BrushSides
+
+# PatchCollision -> CollisionVertices
 
 
 # flag enums:
@@ -107,39 +128,49 @@ class Cell(base.Struct):  # LUMP 17
     mins: vector.vec3
     maxs: vector.vec3
     unknown_1: int  # increments w/ each cell
-    first_portal: int
+    first_portal: int  # index into Portals
     num_portals: int
+    first_cull_group_index: int  # index into CullGroupIndices
+    num_cull_group_indices: int
     unknown_2: bytes
-    __slots__ = ["mins", "maxs", "unknown_1", "first_portal", "num_portals", "unknown_2"]
-    _format = "6f3I16s"  # 52 bytes
+    __slots__ = [
+        "mins", "maxs", "unknown_1", "first_portal", "num_portals",
+        "first_cull_group_index", "num_cull_group_indices", "unknown_2"]
+    _format = "6f5I8s"  # 52 bytes
     _arrays = {"mins": [*"xyz"], "maxs": [*"xyz"]}
     _classes = {"mins": vector.vec3, "maxs": vector.vec3}
 
 
 class CullGroup(base.Struct):  # LUMP 9
-    # indices & aabb tree stuff?
-    unknown: List[int]
-    __slots__ = ["unknown"]
-    _format = "8i"
-    _arrays = {"unknown": 8}
+    mins: vector.vec3
+    maxs: vector.vec3
+    first_unknown: int  # index into ??? (renderables?)
+    num_unknowns: int
+    __slots__ = ["mins", "maxs", "first_unknown", "num_unknowns"]
+    _format = "6f2i"
+    _arrays = {"mins": [*"xyz"], "maxs": [*"xyz"]}
+    _classes = {"mins": vector.vec3, "maxs": vector.vec3}
 
 
 class Leaf(base.Struct):  # LUMP 21
     unknown_1: List[int]
-    first_leaf_brush: int  # index of first LeafBrush in this Leaf
-    num_leaf_brushes: int  # number of LeafBrushes after first_leaf_brush in this Leaf
+    first_leaf_face: int  # index into Leaf Brushes
+    num_leaf_faces: int
+    first_leaf_brush: int  # index into Leaf Brushes
+    num_leaf_brushes: int
     unknown_2: List[int]
-    # LeafFace indices are probably in here somewhere...
-    __slots__ = ["unknown_1", "first_leaf_brush", "num_leaf_brush", "unknown_2"]
+    __slots__ = [
+        "unknown_1", "first_leaf_face", "num_leaf_faces",
+        "first_leaf_brush", "num_leaf_brushes", "unknown_2"]
     _format = "9i"
-    _arrays = {"unknown_1": 4, "unknown_2": 3}
+    _arrays = {"unknown_1": 2, "unknown_2": 3}
 
 
 class Light(base.Struct):  # LUMP 30
     type: LightType
     unknown_1: List[float]  # big floats
-    origin: List[float]  # seems legit
-    vector: List[float]  # magnitude of ~1.0 if a directional type
+    origin: vector.vec3
+    vector: vector.vec3  # magnitude of ~1.0 if a directional type
     unknown_2: List[float]
     unknown_3: List[int]  # indices into other lumps?
     __slots__ = ["type", "unknown_1", "origin", "vector", "unknown_2", "unknown_3"]
@@ -173,32 +204,37 @@ class Lightmap(list):  # LUMP 1
 class Model(base.Struct):  # LUMP 27
     mins: vector.vec3
     maxs: vector.vec3
-    first_face: int  # index into Face lump
-    num_faces: int  # number of Faces after first_face included in this Model
-    first_brush: int  # index into Brush lump
-    num_brushes: int  # number of Brushes after first_brush included in this Model
-    unknown: List[int]
-    __slots__ = ["mins", "maxs", "first_face", "num_faces", "first_brush", "num_brushes", "unknown"]
+    first_triangle_soup: int  # index into TriangleSoups
+    num_triangle_soups: int
+    first_leaf_face: int  # index into LeafFaces
+    num_leaf_faces: int
+    first_brush: int  # index into Brushes
+    num_brushes: int
+    __slots__ = [
+        "mins", "maxs", "first_triangle_soup", "num_triangle_soups",
+        "first_unknown", "num_unknowns", "first_brush", "num_brushes"]
     _format = "6f6i"
-    _arrays = {"mins": [*"xyz"], "maxs": [*"xyz"], "unknown": 2}
+    _arrays = {"mins": [*"xyz"], "maxs": [*"xyz"]}
     _classes = {"mins": vector.vec3, "maxs": vector.vec3}
 
 
 class Occluder(base.Struct):  # LUMP 12
-    first_occluder_plane: int  # index into the OccluderPlane lump
-    num_occluder_planes: int   # number of OccluderPlanes after first_occluder_plane in this Occluder
-    first_occluder_edges: int  # index into the OccluderEdge lump
-    num_occluder_edges: int    # number of OccluderEdges after first_occluder_edge in this Occluder
+    first_occluder_plane: int  # index into OccluderPlanes
+    num_occluder_planes: int
+    first_occluder_edges: int  # index into OccluderEdges
+    num_occluder_edges: int
     __slots__ = ["first_occluder_plane", "num_occluder_planes", "first_occluder_edge", "num_occluder_edges"]
     _format = "4i"
 
 
 class PatchCollision(base.Struct):  # LUMP 24
     """'Patches' are the CoD version of Source's Displacements (think of a fabric patch on torn clothes)"""
-    unknown: List[int]
-    __slots__ = ["unknown"]
-    _format = "16b"
-    _arrays = {"unknown": 16}
+    unknown_1: int  # flags?
+    num_vertices: int
+    unknown_2: int
+    first_vertex: int  # index into CollisionVertices
+    __slots__ = ["unknown_1", "num_vertices", "unknown_2", "first_vertex"]
+    _format = "I2h2I"  # 16 bytes
 
 
 class Portal(base.Struct):  # LUMP 18
@@ -210,58 +246,67 @@ class Portal(base.Struct):  # LUMP 18
     _format = "4i"
 
 
-class TriangleSoup(base.MappedArray):  # LUMP 5
-    texture: int  # index into Textures?
-    draw_order: int  # ?
-    first_vertex: int
+class TriangleSoup(base.Struct):  # LUMP 5
+    texture: int  # index into Textures
+    draw_order: int  # priority?
+    first_vertex: int  # index into Vertices
     num_vertices: int
-    first_triangle: int  # index into Indices?
-    num_triangles: int
-    _mapping = ["texture", "draw_order", "first_vertex", "num_vertices",
-                "first_triangle", "num_triangles"]
+    num_indices: int  # should always be a multiple of 3
+    first_index: int  # index into Indices
+    __slots__ = ["texture", "draw_order", "first_vertex", "num_vertices", "num_indices", "first_index"]
     _format = "2HI2HI"
 
 
 class Vertex(base.Struct):  # LUMP 7
-    # position, uv0 (albedo), uv1 (lightmap), colour etc.
-    unknown: List[int]
-    __slots__ = ["unknown"]
-    _format = "11i"
-    _arrays = {"unknown": 11}
+    position: vector.vec3
+    albedo_uv: vector.vec2
+    lightmap_uv: vector.vec2
+    position: vector.vec3
+    colour: colour.RGBA32
+    __slots__ = ["position", "albedo_uv", "lightmap_uv", "normal", "colour"]
+    _format = "10f4b"
+    _arrays = {
+        "position": [*"xyz"], "albedo_uv": [*"xy"], "lightmap_uv": [*"xy"],
+        "normal": [*"xyz"], "colour": [*"rgba"]}
+    _classes = {
+        "position": vector.vec3, "albedo_uv": vector.vec2, "lightmap_uv": vector.vec2,
+        "normal": vector.vec3, "colour": colour.RGBA32}
 
 
 # {"LUMP_NAME": LumpClass}
-BASIC_LUMP_CLASSES = {"COLLISION_INDICES":  shared.UnsignedShorts,
-                      "CULL_GROUP_INDICES": shared.UnsignedInts,
-                      "INDICES":            shared.UnsignedShorts,
-                      "LEAF_BRUSHES":       shared.UnsignedInts,
-                      "LEAF_FACES":         shared.UnsignedInts,
-                      "LIGHT_INDICES":      shared.UnsignedShorts,
-                      "OCCLUDER_INDICES":   shared.UnsignedShorts,
-                      "OCCLUDER_PLANES":    shared.UnsignedInts}
+BASIC_LUMP_CLASSES = {
+    "COLLISION_INDICES":  shared.UnsignedShorts,
+    "CULL_GROUP_INDICES": shared.UnsignedInts,
+    "INDICES":            shared.UnsignedShorts,
+    "LEAF_BRUSHES":       shared.UnsignedInts,
+    "LEAF_FACES":         shared.UnsignedInts,
+    "LIGHT_INDICES":      shared.UnsignedShorts,
+    "OCCLUDER_INDICES":   shared.UnsignedShorts,
+    "OCCLUDER_PLANES":    shared.UnsignedInts}
 
 LUMP_CLASSES = {
-                # "AABB_TREES":         AxisAlignedBoundingBox,
-                "BRUSHES":            Brush,
-                "BRUSH_SIDES":        BrushSide,
-                "CELLS":              Cell,
-                # "COLLISION_VERTICES": quake.Vertex,
-                # "CULL_GROUPS":        CullGroup,
-                # "VERTICES":           Vertex,
-                "LEAVES":             Leaf,
-                "LIGHTS":             Light,
-                "LIGHTMAPS":          Lightmap,
-                # "MODELS":             Model,
-                "NODES":              quake3.Node,
-                # "OCCLUDERS":          Occluder,
-                "OCCLUDER_EDGES":     quake.Edge,
-                # "PATCH_COLLISION":    PatchCollision,
-                "PLANES":             quake3.Plane,
-                "PORTALS":            Portal,
-                "TEXTURES":           quake3.Texture,
-                "TRIANGLE_SOUPS":     TriangleSoup}
+    # "AABB_TREES":         AxisAlignedBoundingBox,
+    "BRUSHES":            Brush,
+    "BRUSH_SIDES":        BrushSide,
+    "CELLS":              Cell,
+    "COLLISION_VERTICES": quake.Vertex,
+    "CULL_GROUPS":        CullGroup,
+    "VERTICES":           Vertex,
+    "LEAVES":             Leaf,
+    "LIGHTS":             Light,
+    "LIGHTMAPS":          Lightmap,
+    "MODELS":             Model,
+    "NODES":              quake3.Node,
+    # "OCCLUDERS":          Occluder,
+    "OCCLUDER_EDGES":     quake.Edge,
+    "PATCH_COLLISION":    PatchCollision,
+    "PLANES":             quake3.Plane,
+    "PORTALS":            Portal,
+    "TEXTURES":           quake3.Texture,
+    "TRIANGLE_SOUPS":     TriangleSoup}
 
-SPECIAL_LUMP_CLASSES = {"ENTITIES": shared.Entities}
+SPECIAL_LUMP_CLASSES = {
+    "ENTITIES": shared.Entities}
 
 
 def brush(bsp, brush_index) -> editor.Brush:
@@ -285,6 +330,19 @@ def brush(bsp, brush_index) -> editor.Brush:
     return editor.Brush(out)
 
 
+def model(bsp, model_index: int) -> geometry.Model:
+    # entity
+    entities = bsp.ENTITIES.search(model=f"*{model_index}")
+    model_entity = entities[0] if len(entities) != 0 else dict()
+    origin = model_entity.get("origin", "0 0 0")
+    origin = vector.vec3(*origin.split())
+    # NOTE: haven't found any model ents w/ angles
+    # geometry
+    model = bsp.MODELS[model_index]
+    start, length = model.first_triangle_soup, model.num_triangle_soups
+    return geometry.Model([bsp.triangle_soup_mesh(i) for i in range(start, start + length)], origin)
+
+
 def portal_file(bsp) -> str:
     out = ["PRT1", str(len(bsp.CELLS)), str(len(bsp.PORTALS))]
     for ci, cell in enumerate(bsp.CELLS):
@@ -297,5 +355,23 @@ def portal_file(bsp) -> str:
     return "\n".join(out)
 
 
-methods = [quake.leaves_of_node, shared.worldspawn_volume, brush, portal_file]
+def triangle_soup_mesh(bsp, triangle_soup_index: int) -> geometry.Mesh:
+    triangle_soup = bsp.TRIANGLE_SOUPS[triangle_soup_index]
+    # material
+    texture = bsp.TEXTURES[triangle_soup.texture]
+    material = geometry.Material(texture.name.split(b"\0")[0].decode())
+    # geometry
+    start, length = triangle_soup.first_vertex, triangle_soup.num_vertices
+    vertices = bsp.VERTICES[start:start + length]
+    vertices = [
+        geometry.Vertex(v.position, v.normal, v.albedo_uv, v.lightmap_uv, colour=v.colour.as_floats())
+        for v in vertices]
+    start, length = triangle_soup.first_index, triangle_soup.num_indices
+    indices = bsp.INDICES[start:start + length]
+    triangles = [(indices[i], indices[i + 1], indices[i + 2]) for i in range(0, length, 3)]
+    polygons = [geometry.Polygon([vertices[a], vertices[b], vertices[c]]) for a, b, c in triangles]
+    return geometry.Mesh(material, polygons)
+
+
+methods = [quake.leaves_of_node, shared.worldspawn_volume, brush, model, portal_file, triangle_soup_mesh]
 methods = {m.__name__: m for m in methods}
