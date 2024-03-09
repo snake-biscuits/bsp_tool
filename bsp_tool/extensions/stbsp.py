5ea4e88d4460554d6f97258c1b08b05eca1b0310
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2024-03-10 08:47:40 +1000

    (extensions.stbsp) cleanup

--- bsp_tool/extensions/stbsp.py
+++ bsp_tool/extensions/stbsp.py
@@ -13,15 +13,14 @@ def read_struct(file, format_: str) -> List[Any]:
 BlockIndex = collections.namedtuple("BlockIndex", ["offset", "size"])
 
 
-class BlockType(enum.Enum):
-    MATERIALS = 0  # zero-terminated strings
-    # TODO: do other lumps index first byte of each material?
-    UNKNOWN_1 = 1  # 24 byte structs
+class Block(enum.Enum):
+    # observed order: 1, 2, 3, 0, 4, 5
+    MATERIALS = 0
+    MATERIAL_INFO = 1
     VTFS = 2  # uint32_t
     VMTS = 3  # uint16_t
-    UNKNOWN_4 = 4  # 24 byte structs
-    UNKNOWN_5 = 5  # mip pixels?
-# observed order: 1, 2, 3, 0, 4, 5
+    COLUMNS = 4  # columns
+    COLUMN_DATA = 5  # column data
 
 
 class FlatStruct:
@@ -46,47 +45,55 @@ class FlatStruct:
         return cls.from_bytes(stream.read(struct.calcsize(cls._format)))
 
 
-class Block1(FlatStruct):  # UNKNOWN_1
+class MaterialInfo(FlatStruct):
     """rpak assets & vmts?"""
-    material: int  # index into raw_materials (first byte of string)
-    # stbsp.materials[stbsp.material_index[block_1.material]]
+    material: int  # index materials
     unknown_1: int  # flags?
-    hash: int  # indexes rpak?
-    # NOTE: only index vmt / vtf if hash is 0
+    guid: int  # indexes rpak?
+    # NOTE: only index vmt & vtf if guid == 0
     vmt: int  # index into vmts
     vtf: int  # index into vtfs
-    __slots__ = ["material", "unknown_1", "hash", "vmt", "vtf"]
+    __slots__ = ["material", "unknown_1", "guid", "vmt", "vtf"]
     _format = "2IQ2I"
 
+    def __repr__(self) -> str:
+        args = ", ".join([
+            str(self.material), str(self.unknown_1),
+            f"0x{self.guid:016X}",
+            str(self.vmt), str(self.vtf)])
+        return f"MaterialInfo({args})"
 
-class Block4(FlatStruct):  # UNKNOWN_4
+
+class Column(FlatStruct):
     """grid tile data?"""
-    offset: int  # index into UNKNOWN_5?
+    offset: int  # index into ColumnData
     length: int
-    flags: int  # maybe asset type?
-    first_x: int  # stbsp.block_4[0].first_x == stbsp.unknown[0]
-    first_y: int  # stbsp.block_4[0].first_y == stbsp.unknown[1]
-    last_x: int  # stbsp.block_4[-1].first_x + last_x == stbsp.unknown[3]
-    last_y: int  # stbsp.block_4[-1].first_y + last_y == stbsp.unknown[4]
-    __slots__ = [
-        "offset", "length", "flags",
-        "first_x", "first_y", "last_x", "last_y"]
+    colour: int  # average histogram colour (RGBA32)
+    mins_x: int
+    mins_y: int
+    maxs_x: int
+    maxs_y: int
+    __slots__ = ["offset", "length", "colour", "mins_x", "mins_y", "maxs_x", "maxs_y"]
     _format = "Q2I4h"
 
 
 class StreamBsp:
     # header
-    unknown: List[int]
-    scale: List[float]
+    mins_x: int
+    mins_y: int
+    maxs_x: int
+    maxs_y: int
+    stride: int  # width / height of each column
+    scale: List[float]  # width, height
     block_indices: List[BlockIndex]
     # data
     materials: Dict[int, str]
     # ^ {first_byte: material}
-    block_1: List[Block1]
-    vtfs: List[int]  # indexes materials; vtf filenames
-    vmts: List[int]  # indexes vtfs; albedo for vmt
-    block_4: List[Block4]
-    block_5: bytes
+    material_info: List[MaterialInfo]
+    vtfs: List[int]  # indexes materials
+    vmts: List[int]  # indexes vtfs
+    columns: List[Column]
+    column_data: bytes
 
     def __repr__(self) -> str:
         return f"<StreamBsp {len(self.materials)} materials @ 0x{id(self):016X}>"
@@ -99,48 +106,47 @@ class StreamBsp:
             version = read_struct(file, "2H")
             assert version == (8, 0)  # r2 only; r5 s3 is (8, 1)
             out = cls()
-            out.unknown = read_struct(file, "2i3I")
+            out.mins_x, out.mins_y, out.maxs_x, out.maxs_y, out.stride = read_struct(file, "4iI")
             out.scale = read_struct(file, "2f")
             assert read_struct(file, "33I") == (0,) * 33
             out.block_indices = [BlockIndex(*read_struct(file, "2Q")) for i in range(6)]
             assert read_struct(file, "16Q") == (0,) * 16
             assert file.tell() == min(bi.offset for bi in out.block_indices)
-            # Block0: materials
-            materials_block = out.block_indices[0]
-            file.seek(materials_block.offset)
-            raw_materials = file.read(materials_block.size)
+            # Block 0: Materials
+            file.seek(out.block_indices[0].offset)
+            raw_materials = file.read(out.block_indices[0].size)
             assert raw_materials.endswith(b"\0")
             material_strings = [s.decode() for s in raw_materials[:-1].split(b"\0")]
             out.materials, i = {0: material_strings[0]}, 0
             while len(out.materials) < raw_materials.count(b"\0"):
                 i = raw_materials.find(b"\0", i) + 1
                 out.materials[i] = material_strings[len(out.materials)]
-            # Block1
+            # Block 1: Material Info
             file.seek(out.block_indices[1].offset)
-            out.block_1 = [Block1.from_stream(file) for i in range(out.block_indices[1].size)]
-            # Block2: vtfs
+            out.material_info = [MaterialInfo.from_stream(file) for i in range(out.block_indices[1].size)]
+            # Block 2: VTFs
             file.seek(out.block_indices[2].offset)
             out.vtfs = read_struct(file, f"{out.block_indices[2].size}I")
-            # Block3: vmts
+            # Block 3: VMTs
             file.seek(out.block_indices[3].offset)
             out.vmts = read_struct(file, f"{out.block_indices[3].size}H")
-            # Block4
+            # Block 4: Columns
             file.seek(out.block_indices[4].offset)
-            out.block_4 = [Block4.from_stream(file) for i in range(out.block_indices[4].size)]
-            # Block5
+            out.columns = [Column.from_stream(file) for i in range(out.block_indices[4].size)]
+            # Block 4: Column Data
             file.seek(out.block_indices[5].offset)
-            out.block_5 = file.read(out.block_indices[5].size)
+            out.column_data = file.read(out.block_indices[5].size)
             # TODO: assert EOF
         return out
 
-    def deadzones_as_obj(self, filename: str):
+    def as_obj(self, filename: str):
         x_scale, y_scale = self.scale
         with open(filename, "w") as obj_file:
             obj_file.write("# generated by bsp_tool.extensions.stbsp\n")
-            for b in self.block_4:
-                if b.flags == 0x00800000:
-                    obj_file.write(f"v {b.first_x * x_scale} {b.first_y * y_scale} 0\n")
-                    obj_file.write(f"v {b.first_x * x_scale} {b.last_y * y_scale} 0\n")
-                    obj_file.write(f"v {b.last_x * x_scale} {b.last_y * y_scale} 0\n")
-                    obj_file.write(f"v {b.last_x * x_scale} {b.first_y * y_scale} 0\n")
-                    obj_file.write("f -1 -2 -3 -4\n")
+            for column in self.columns:
+                obj_file.write(f"usemtl {column.colour:08X}\n")
+                obj_file.write(f"v {column.mins_x * x_scale} {column.mins_y * y_scale} 0\n")
+                obj_file.write(f"v {column.mins_x * x_scale} {column.maxs_y * y_scale} 0\n")
+                obj_file.write(f"v {column.maxs_x * x_scale} {column.maxs_y * y_scale} 0\n")
+                obj_file.write(f"v {column.maxs_x * x_scale} {column.mins_y * y_scale} 0\n")
+                obj_file.write("f -1 -2 -3 -4\n")
