b3f86d7df43464d0d022617e6eb50274d14afd65
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2024-11-11 13:15:51 +1000

    (respawn.RespawnBsp) updating docstring

--- bsp_tool/respawn.py
+++ bsp_tool/respawn.py
@@ -164,7 +164,7 @@ class ExternalLumpManager:
 
 
 class RespawnBsp(valve.ValveBsp):
-    """Respawn Entertainment's Titanfall Engine .bsp (rBSP v29 -> 51.1)"""
+    """Respawn Entertainment's Titanfall Engine .bsp (rBSP v29 -> 52.1)"""
     # https://developer.valvesoftware.com/wiki/Source_BSP_File_Format/Game-Specific#Titanfall
     # https://raw.githubusercontent.com/Wanty5883/Titanfall2/master/tools/TitanfallMapExporter.py
     endianness: str = "little"
