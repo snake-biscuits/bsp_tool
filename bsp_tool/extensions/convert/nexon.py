05e7ac5f4a548d008beb941d368ae80eafa9a894
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2025-03-23 15:08:50 +1000

    (#207) 'convert' maps with no `GAME_LUMP`

--- bsp_tool/extensions/convert/nexon.py
+++ bsp_tool/extensions/convert/nexon.py
@@ -8,6 +8,11 @@ from ...branches import valve
 def vindictus_steam_to_69(bsp: ValveBsp, outdir: str = "./"):
     assert isinstance(bsp, ValveBsp)
     assert bsp.branch == nexon.vindictus
+
+    if not hasattr(bsp, "GAME_LUMP") or "sprp" not in bsp.GAME_LUMP.headers:
+        # no conversion needed, just copy the map
+        bsp.save_as(os.path.join(outdir, bsp.filename))
+
     assert bsp.GAME_LUMP.headers["sprp"].version in (6, 7, 8)
 
     def downgrade(prop):
