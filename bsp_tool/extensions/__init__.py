cb2d3c7197adbd15b64f471624a7625eb9b49731
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2025-05-05 15:57:55 +1000

    (#208) remove `extensions.to_cpp`

--- bsp_tool/extensions/__init__.py
+++ bsp_tool/extensions/__init__.py
@@ -1,6 +1,6 @@
 __all__ = [
     "compiler_signature", "convert", "decompile_rbsp",
-    "diff", "editor", "mprt", "stbsp", "to_cpp"]
+    "diff", "editor", "mprt", "stbsp"]
 
 from . import compiler_signature  # identify signatures in .bsp files
 from . import convert  # .bsp format conversion
@@ -9,7 +9,6 @@ from . import diff  # compare 2 .bsps & their lumps
 from . import editor  # parse editor formats (.map, .vmf etc.)
 from . import mprt  # hacky fix for loading static props in Blender (RespawnBsp)
 from . import stbsp  # RespawnBsp .rpak mip caching helper
-from . import to_cpp  # bulk convert a whole branch_script to C/C++ types
 
 # NOTE: apex_archive & decrypt_xor are meant to be run directly
 # -- apex_archive is for managing & searching file hashes
